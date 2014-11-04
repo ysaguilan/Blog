764f187b6982cdfcf004812527969a647b92f037
yulitza saguilan <yulitza.saguilan@gmail.com>
2014-11-04 08:56:04 -0800

    Created a Navigation View: created navigation php file and post php file in view folder

--- amit1.php
+++ amit1.php
@@ -3,6 +3,8 @@
 <?php
 /*inserts header code into this php file*/
 require_once(__DIR__ . "/view/header.php");
+
+require_once(__DIR__ . "/view/navigation.php");
 /*includes information in database.php within create-db.php*/
 require_once(__DIR__ . "/controller/create-db.php");
 /*inserts footer code into this php file*/
---
+++ view/navigation.php
@@ -0,0 +1,5 @@
+<nav>
+	<ul>
+		<li><a href="../post.php"> Blog Post Forum</a></li>
+	</ul>
+	</nav>
