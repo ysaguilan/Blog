1ad4dc33ab145988f65f7ca7b5d61319d9ee7069
yulitza saguilan <yulitza.saguilan@gmail.com>
2014-11-06 09:16:23 -0800

    Added Media query

--- view/header.php
+++ view/header.php
@@ -1,5 +1,6 @@
 <html>
 <!--link to css file to make it look fancy-->
 <link rel="stylesheet" type="text/css" href="css/header.css">
+<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
 <head><title>Amit4 php</title></head>
 <body>
