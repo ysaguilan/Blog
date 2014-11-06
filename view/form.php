3c16d58bb151b3f90b06d6667e9bb2b5835c0c03
yulitza saguilan <yulitza.saguilan@gmail.com>
2014-11-05 19:29:46 -0800

    Created css file for header.php

--- view/form.php
+++ view/form.php
@@ -1,12 +1,16 @@
+<!--creates header with bigges font size-->
 <h1>
 	Create Blog Post
 </h1>
+<!--takes in certain information and sends it to the table-->
+<!--used to create an HTML form-->
 <form>
+<!--makes a title text box to create a title for posts-->
 <div>
 <label for="title">Title: </label>
 <input type="text" name="title"/>
 </div>
-
+<!--make a posts text box to create a multi word post-->
 <div>
 <label for="post">Post: </label>
 <textarea name="post"></textarea>	
--- view/header.php
+++ view/header.php
@@ -1,4 +1,5 @@
 <html>
+<!--link to css file to make it look fancy-->
 <link rel="stylesheet" type="text/css" href="css/header.css">
 <head><title>Amit4 php</title></head>
 <body>
