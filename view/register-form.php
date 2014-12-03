1a53079f75ac0a0109fe1c26aa017899ec73c0d2
yulitza saguilan <yulitza.saguilan@gmail.com>
2014-12-02 19:29:21 -0800

    Created a Registration Form: for users to register and have access to post stuff

---
+++ view/register-form.php
@@ -0,0 +1,24 @@
+<!--creates a form for registration-->
+<form>
+
+<!--creates text space for email-->
+	<div>
+	<label for="email">Email:</label>
+	<input type="text" name="email" />
+	</div>
+	<!--creates textspace for username-->
+	<div>
+	<label for="username">Username:</label>
+	<input type="text" name="username"/>
+	</div>
+<!--creates a text space for password-->
+	<div>
+	<label for="password">Password:</label>	
+	<!--type is password so that little black dots appear instead of letters when user is typing a password-->
+	<input type="password" name="password"/>
+	</div>
+<!--creates button called submit-->
+	<div>
+	<button type="submit">Submit</button>
+	</div>
+</form>
