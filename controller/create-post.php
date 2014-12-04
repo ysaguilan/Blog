9e1616047fa5a220dd499dbc763b99d27d0be3ed
yulitza saguilan <yulitza.saguilan@gmail.com>
2014-12-04 09:05:05 -0800

    Like previous commit but with the missing comment

--- controller/create-post.php
+++ controller/create-post.php
@@ -7,14 +7,16 @@ require_once(__DIR__ . "/../model/config.php");
 $title = filter_input(INPUT_POST, "title", FILTER_SANITIZE_STRING);
 /*filters input to make sure there's no malicuos things occuring in th input for post*/
 $post = filter_input(INPUT_POST, "post", FILTER_SANITIZE_STRING);
-
+/*holds the current time */
 $date = new DateTime('today');
+/*holds the current time in Los Angeles*/
 $time = new DateTime('America/Los_Angeles');
 
 $query = $_SESSION["connection"]->query("INSERT INTO posts SET title = '$title', post = '$post'");
 
 if ($query) {
 	echo "<p>successfully inserted post:$title</p>";
+	/*echos date and time (current)*/                                 /*formats how time looks*/
 	echo "Posted on: " . $date->format("M/D" ." " . "d/Y") . " at " . $time->format("g:i");
 }
 else{
