33f9c4594915317e8bd5db8152012e4360183384
yulitza saguilan <yulitza.saguilan@gmail.com>
2014-12-08 19:25:40 -0800

    Created a Salt for Password

--- controller/create-user.php
+++ controller/create-user.php
@@ -5,4 +5,8 @@ $email = filter_input(INPUT_POST, "email", FILTER_SANITIZE_EMAIL);
 $username = filter_input(INPUT_POST, "username", FILTER_SANITIZE_STRING);
 $password = filter_input(INPUT_POST, "password", FILTER_SANITIZE_STRING);
 
-echo $email . " - " . $username . " - " . $password;
+echo $password;
+/*tells php to use sha256 creates 5000 rounds*/
+$salt = "$5$" . "rounds=5000$" . uniqid(mt_rand(), true) . "$";
+
+echo $salt;
