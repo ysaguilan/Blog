0e02e276c8ddf4b19cf76fa2046ab2583e5b7c4e
yulitza saguilan <yulitza.saguilan@gmail.com>
2014-12-08 20:40:13 -0800

    Encrypted Password and Stored it in the Database

--- controller/create-user.php
+++ controller/create-user.php
@@ -5,8 +5,20 @@ $email = filter_input(INPUT_POST, "email", FILTER_SANITIZE_EMAIL);
 $username = filter_input(INPUT_POST, "username", FILTER_SANITIZE_STRING);
 $password = filter_input(INPUT_POST, "password", FILTER_SANITIZE_STRING);
 
-echo $password;
 /*tells php to use sha256 creates 5000 rounds*/
 $salt = "$5$" . "rounds=5000$" . uniqid(mt_rand(), true) . "$";
 
-echo $salt;
+$hashedPassword = crypt($password, $salt);
+
+$query = $_SESSION["connection"]->query("INSERT INTO users SET "
+	. "email = '$email',"
+	. "username ='$username'," 
+	. "password = '$hashedPassword',"
+	. "salt = '$salt'");
+
+if($query){
+	echo "sucessfully created user: $username";
+}
+else{
+	echo "<p>" . $_SESSION["connection"]->error . "</p>";
+}	
