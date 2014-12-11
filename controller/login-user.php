7be01e75b871a05f064cbe8b079ea08eeb1a822d
yulitza saguilan <yulitza.saguilan@gmail.com>
2014-12-11 09:18:59 -0800

    Checking the Passwords for Login: -errors

--- controller/login-user.php
+++ controller/login-user.php
@@ -5,3 +5,17 @@ $username = filter_input(INPUT_POST, "username", FILTER_SANITIZE_STRING);
 $password = filter_input(INPUT_POST, "password", FILTER_SANITIZE_STRING);
 
 $query = $_SESSION["connection"]->query("SELECT salt, password FROM users WHERE username = '$username'");
+if($query->num_rows == 1){
+	$row = $query->fetch_array();
+
+	if ($row["password"] == crypt($password, $row["$salt"])) {
+		echo "<p>Login successful</p>";
+	}
+	else{
+		echo "<p>Invalid Username and Password</p>";
+	}	
+}
+else{
+		echo "<p>Invalid Username and Password</p>";
+	}
+?>
